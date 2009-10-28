9c579564ff6262f76efda70333f4e6bddf75f330
Jeroen Tietema <jeroen@voidwalkers.nl>
2009-10-28 21:39:00 +0100

    license headers and some documentation

--- application/controllers/AdminPagesController.php
+++ application/controllers/AdminPagesController.php
@@ -1,7 +1,32 @@
 <?php
+/**
+ * Class AdminPagesController
+ * @package Fizzy
+ * @subpackage Controller
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.voidwalkers.nl/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@voidwalkers.nl so we can send you a copy immediately.
+ *
+ * @copyright Copyright (c) 2009 Voidwalkers (http://www.voidwalkers.nl)
+ * @license http://www.voidwalkers.nl/license/new-bsd The New BSD License
+ */
 
+/** SecureController */
 require_once 'SecureController.php';
 
+/**
+ * Admin controller for managing the pages
+ *
+ * @author Jeroen Tietema <jeroen@voidwalkers.nl>
+ * @author Mattijs Hoitink <mattijs@voidwalkers.nl>
+ */
 class AdminPagesController extends SecureController
 {
 
@@ -72,6 +97,9 @@ class AdminPagesController extends SecureController
         $this->getView()->setScript('admin/page/form.phtml');
     }
 
+    /**
+     * Edits a page
+     */
     public function editAction()
     {
         $application = Fizzy_Config::getInstance()->getSection(Fizzy_Config::SECTION_APPLICATION);
@@ -131,6 +159,9 @@ class AdminPagesController extends SecureController
         $this->getView()->setScript('admin/page/form.phtml');
     }
 
+    /**
+     * Shows a delete confirmation and deletes if Yes is pressed
+     */
     public function deleteAction()
     {
         $config = Fizzy_Config::getInstance();
