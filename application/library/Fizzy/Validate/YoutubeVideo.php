9fa2557f859f21afac052e578fe252d26ded199b
Mattijs Hoitink <mattijs@voidwalkers.nl>
2010-03-05 14:59:39 +0100

    Twitter account validator
    Added validator for twitter accounts;
    Fixed docblocks for YouTube Video validator;

--- application/library/Fizzy/Validate/YoutubeVideo.php
+++ application/library/Fizzy/Validate/YoutubeVideo.php
@@ -1,8 +1,34 @@
 <?php
+/**
+ * Class Fizzy_Validate_YoutubeVideo
+ * @category Fizzy
+ * @package Fizzy_Validate
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.voidwalkers.nl/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@voidwalkers.nl so we can send you a copy immediately.
+ *
+ * @copyright Copyright (c) 2009-2010 Voidwalkers (http://www.voidwalkers.nl)
+ * @license http://www.voidwalkers.nl/license/new-bsd The New BSD License
+ */
 
+/** Zend_Validate_Abstract */
+require_once 'Zend/Validate/Abstract.php';
+
+/** Zend_Uri_Http */
+require_once 'Zend/Uri/Http.php';
+
+/** Zend_Http_Client */
+require_once 'Zend/Http/Client.php';
 
 /**
- * validator for YouTube video's. Uses the status code of an HTTP request to
+ * Validator for YouTube video's. Uses the status code of an HTTP request to
  * to the YouTube API to verify a video exists.
  *
  * API URI: http://gdata.youtube.com/feeds/api/videos/VIDEO_ID
@@ -11,10 +37,14 @@
  */
 class Fizzy_Validate_YoutubeVideo extends Zend_Validate_Abstract
 {
+
     const API_URL = 'http://gdata.youtube.com/feeds/api/videos/';
     const INVALID_URI = 'invalidUri';
     const INVALID_VIDEO = 'notAVideo';
 
+    /**
+     * @see Zend_Validate_Abstract
+     */
     protected $_messageTemplates = array(
         self::INVALID_URI => "'%value%' is not a valid url",
         self::INVALID_VIDEO => "'%value%' is not a valid Youtube video"
