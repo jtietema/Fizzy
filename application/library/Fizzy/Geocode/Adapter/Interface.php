700b22296c41c8207ea949e63895e1fbfe03fcdf
Jeroen Tietema <jeroen@voidwalkers.nl>
2010-04-16 10:35:49 +0200

    geocode adapter interface aangepast

--- application/library/Fizzy/Geocode/Adapter/Interface.php
+++ application/library/Fizzy/Geocode/Adapter/Interface.php
@@ -3,6 +3,6 @@ interface Fizzy_Geocode_Adapter_Interface
 {
     public function getQuery();
     public function setQuery($query);
-    public function location($query);
+    public function location($query = null);
     
 }
