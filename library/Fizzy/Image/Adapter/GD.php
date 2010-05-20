d9bb5759c46257d2789625d9397051013d585780
Jeroen Tietema <jeroen@voidwalkers.nl>
2010-05-20 17:27:44 +0200

    updated copyright year

--- library/Fizzy/Image/Adapter/GD.php
+++ library/Fizzy/Image/Adapter/GD.php
@@ -13,7 +13,7 @@
  * obtain it through the world-wide-web, please send an email
  * to license@voidwalkers.nl so we can send you a copy immediately.
  *
- * @copyright Copyright (c) 2009 Voidwalkers (http://www.voidwalkers.nl)
+ * @copyright Copyright (c) 2009-2010 Voidwalkers (http://www.voidwalkers.nl)
  * @license http://www.voidwalkers.nl/license/new-bsd The New BSD License
  */
 
