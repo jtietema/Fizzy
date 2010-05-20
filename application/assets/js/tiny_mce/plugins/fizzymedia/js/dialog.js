c192cce9399abbad8929ea36e8e58a91753e95e7
Jeroen Tietema <jeroen@voidwalkers.nl>
2010-05-20 17:23:42 +0200

    added lots of missing license headers

--- application/assets/js/tiny_mce/plugins/fizzymedia/js/dialog.js
+++ application/assets/js/tiny_mce/plugins/fizzymedia/js/dialog.js
@@ -1,3 +1,20 @@
+/**
+ * TinyMCE plugin for the Fizzy Media Library
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.voidwalkers.nl/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@voidwalkers.nl so we can send you a copy immediately.
+ *
+ * @author Jeroen Tietema <jeroen@voidwalkers.nl>
+ * @copyright Copyright 2009, Voidwalkers, All rights reserved.
+ */
+
 tinyMCEPopup.requireLangPack();
 
 var selectedImage = null;
--- library/Fizzy/Resource/Doctrine.php
+++ library/Fizzy/Resource/Doctrine.php
@@ -1,4 +1,22 @@
 <?php
+/**
+ * Class Fizzy_Resource_Doctrine
+ * @category Fizzy
+ * @package Fizzy_Resource
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.voidwalkers.nl/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@voidwalkers.nl so we can send you a copy immediately.
+ *
+ * @copyright Copyright (c) 2009 Voidwalkers (http://www.voidwalkers.nl)
+ * @license http://www.voidwalkers.nl/license/new-bsd The New BSD License
+ */
 
 /**
  * Resource for bootstrapping Doctrine
